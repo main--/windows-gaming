c09d3d2a356238c1c27895f9a12d12b44e7ee13c
main() <main@ehvag.eu.org>
2017-05-19 16:46:53 +0200

    Need to kill the GA when uninstalling
    Or the delete fails

--- guest-agent/uninstall.bat
+++ guest-agent/uninstall.bat
@@ -1,5 +1,6 @@
 @echo off
 reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /f /v WindowsGamingGA
+taskkill /im VfioService.exe
 del %APPDATA%\WindowsGamingGA\VfioService.exe
 rmdir %APPDATA%\WindowsGamingGA
 echo Uninstall complete.
