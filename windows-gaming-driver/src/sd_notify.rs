5e033900b28fd4455dbebfd8005cc17ade5a224b
main() <main@ehvag.de>
2017-06-01 14:23:49 +0200

    Handroll sd_notify
    So we can get rid of the git dependency and no longer link directly to libsystemd (making it an optional dependency).

--- windows-gaming-driver/src/sd_notify.rs
+++ windows-gaming-driver/src/sd_notify.rs
@@ -1,12 +1,29 @@
-use systemd::daemon::*;
-use std::collections::HashMap;
+use libc::{dlopen, dlsym, c_char, RTLD_LAZY};
+use std::ffi::CString;
+use std::mem::transmute;
 
+type SdNotify = Option<extern "C" fn(i32, *const c_char) -> i32>;
+
+lazy_static! {
+    static ref SD_NOTIFY: SdNotify = {
+        unsafe {
+            let lib = CString::new("libsystemd.so").unwrap();
+            let dll = dlopen(lib.as_ptr(), RTLD_LAZY);
+            if dll.is_null() {
+                None
+            } else {
+                let symbol = CString::new("sd_notify").unwrap();
+                transmute(dlsym(dll, symbol.as_ptr()))
+            }
+        }
+    };
+}
+
+/// Attempts to notify systemd about our status.
+/// Doesn't do anything unless we're running as a systemd service.
 pub fn notify_systemd(ready: bool, status: &'static str) {
-    let mut info = HashMap::new();
-    info.insert(STATE_READY, if ready { "1" } else { "0" });
-    info.insert(STATE_STATUS, status);
-
-    // this returns false if we're not actually running inside systemd
-    // we don't care about that though
-    notify(false, info).expect("sd_notify failed");
+    if let Some(sd_notify) = *SD_NOTIFY {
+        let state = CString::new(format!("STATE_READY={}\nSTATE_STATUS={}", if ready { "1" } else { "0" }, status)).unwrap();
+        sd_notify(1, state.as_ptr());
+    }
 }
