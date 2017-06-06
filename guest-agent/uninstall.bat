f10da4c086a060f8d186a6dd1dea5873679c9961
main() <main@ehvag.eu.org>
2017-06-06 13:41:36 +0200

    Fix argument order

--- guest-agent/uninstall.bat
+++ guest-agent/uninstall.bat
@@ -1,6 +1,6 @@
 @echo off
 reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /f /v WindowsGamingGA
-taskkill /im /f VfioService.exe
+taskkill /f /im VfioService.exe
 del %APPDATA%\WindowsGamingGA\VfioService.exe
 rmdir %APPDATA%\WindowsGamingGA
 echo Uninstall complete.
