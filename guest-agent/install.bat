349b4d44f3ea0d2eaa11f6a7d7c68723119f475c
main() <main@ehvag.de>
2017-08-08 21:19:26 +0200

    Fix missing quotes

--- guest-agent/install.bat
+++ guest-agent/install.bat
@@ -1,5 +1,5 @@
 @echo off
-copy Loader.exe %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\VfioLoader.exe
+copy Loader.exe "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\VfioLoader.exe"
 start VfioService.exe
 echo Installation complete.
 pause
