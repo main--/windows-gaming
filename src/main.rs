8d17bff6890bf77f7a8fdd6b635e3736bdb362fa
main() <main@ehvag.de>
2017-09-19 13:43:05 +0200

    Make runtime directory configurable so you actually can run multiple instances

--- src/main.rs
+++ src/main.rs
@@ -125,6 +125,11 @@ fn main() {
         _ => DATA_FOLDER,
     }).to_owned();
 
+    let workdir_path = match cfg {
+        Some(Config { runtime_directory_override: Some(ref x), .. }) => Path::new(x).to_path_buf(),
+        _ => workdir_path,
+    };
+
     match matches.subcommand() {
         ("run", _) => driver::run(&cfg.unwrap(), &workdir_path, &data_folder),
         ("wizard", _) => wizard::run(cfg, &config_path, &workdir_path, &data_folder),
