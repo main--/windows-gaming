a06e9dfbd9d18fcc90a4214c481d29001d573937
main() <main@ehvag.de>
2021-11-17 12:23:43 +0100

    Fix sd_notify always sending Ready

--- windows-gaming/driver/src/sd_notify.rs
+++ windows-gaming/driver/src/sd_notify.rs
@@ -5,7 +5,11 @@ use self::api::NotifyState;
 /// Doesn't do anything unless we're running as a systemd service.
 pub fn notify_systemd(ready: bool, status: &'static str) {
     trace!("Notifying systemd (ready={} status='{}')", ready, status);
-    if let Err(e) = api::notify(false, &[NotifyState::Ready, NotifyState::Status(status.to_owned())]) {
+    let mut state = vec![NotifyState::Status(status.to_owned())];
+    if ready {
+        state.push(NotifyState::Ready);
+    }
+    if let Err(e) = api::notify(false, &state) {
         debug!("sd_notify error: {:?}", e);
     }
 }
